6bfc6d1beb38c84cfb483a1957a77319843e0c34
asburlan <andreisburlan@gmail>
2025-01-19 02:31:54 -0500

    not working but need 2 save

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import numpy as np
 
+# Initialize connection.
+conn = st.connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * FROM breach WHERE uri = "https://roblox.com" ORDER BY id DESC LIMIT 10;', ttl=600)
+
+
+
 dataframe = np.random.randn(10, 20)
 st.dataframe(dataframe)
